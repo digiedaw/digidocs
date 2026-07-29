1c0ac95300e75b560a9def8269d1787ce57ce53c
idoslol3 <lsanyar6@gmail.com>
2026-07-29 16:00:16 +0700

    Deploy website - based on cbd3e74a8a6d48fc38babdc6072d63f23b1d8e05

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Users\\hhhhhhhhhhhhhhhhhhhh\\desktop\\project\\dmworld\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\hhhhhhhhhhhhhhhhhhhh\\desktop\\project\\dmworld\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\hhhhhhhhhhhhhhhhhhhh\\desktop\\project\\dmworld\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\hhhhhhhhhhhhhhhhhhhh\\desktop\\project\\dmworld\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\hhhhhhhhhhhhhhhhhhhh\\desktop\\project\\dmworld\\src\\css\\custom.css"),
-];
